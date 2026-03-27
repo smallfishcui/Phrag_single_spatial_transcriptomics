3c4b285b5eb22fc805d7876540d6f161f1fe1b8e
CWang_ecoevo <41078885+smallfishcui@users.noreply.github.com>
2026-03-27 10:32:54 +0200

    Update step8_spatial_projection_seurat.sh

--- step8_spatial_projection_seurat.sh
+++ step8_spatial_projection_seurat.sh
@@ -24,6 +24,7 @@ pdf("integrated_clusters_allsix_spatial_.pdf")
 SpatialFeaturePlot(spatial, features = c("0", "1","2","3","4","5","6","7","8"), pt.size.factor = 7, ncol = 2, crop = TRUE)
 dev.off()
 
+##################################################################################################################
 anchors <- FindTransferAnchors(reference = seurat_obj_all.list[[1]], query = spatial, normalization.method = "SCT" )
 predictions.assay <- TransferData(anchorset = anchors, refdata = seurat_obj_all_combined$seurat_clusters, prediction.assay = TRUE,
     weight.reduction = spatial[["pca"]], dims = 1:30)
@@ -32,7 +33,9 @@ spatial[["predictions"]] <- predictions.assay
 pdf("integrated_clusters_allsix_spatial_.pdf")
 SpatialFeaturePlot(spatial, features = c("0", "1","2","3","4","5","6","7","8"), pt.size.factor = 7, ncol = 2, crop = TRUE)
 dev.off()
+############################################################################################################################
 
+####################################################################################################################
 features <- SelectIntegrationFeatures(object.list = list(seurat_obj_all.list[[6]], spatial), nfeatures = 3000)
 anchors <- FindTransferAnchors(reference = seurat_obj_all.list[[6]], query = spatial, normalization.method = "SCT" )
 refdata <- seurat_obj_all.list[[6]]@meta.data$harmony_clusters_0.8
@@ -52,3 +55,4 @@ SpatialFeaturePlot(spatial, features = c("0", "1","2","3"), pt.size.factor = 7, 
   legend.title = element_text(size = 8) # Adjust title size (if needed)
 )
 dev.off()
+###################################################################################################################
